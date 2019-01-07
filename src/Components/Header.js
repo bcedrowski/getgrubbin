92e2633f549d43f783f3bde731ef9358be226e0e
Brian Cedrowski <bcedrowski@gmail.com>
2019-01-07 12:33:29 -0500

    recipe app react

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header () {
+    return(
+        <div className="container">
+        <header>
+            <img
+                src="https://www.spectrumgenerations.org/uploads/originals/nutrition-home-page-banner-homepagebanner.jpg"
+                />
+            <div className="top-left">fOOd juNKie</div>
+        </header>
+        </div>
+    )
+}
+
+export default Header
